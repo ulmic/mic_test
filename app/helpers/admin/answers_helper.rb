32500496327d07b1832351ca28ec99ca0570aba8
pavel <kalashnikovisme@gmail.com>
2014-01-16 05:57:21 +0400

    add answers checking

--- app/helpers/admin/answers_helper.rb
+++ app/helpers/admin/answers_helper.rb
@@ -1,2 +1,11 @@
 module Admin::AnswersHelper
+  def points(answers)
+    points = 0
+    answers.each do |answer|
+      if answer.check == :right
+        points += 1
+      end
+    end
+    points
+  end
 end
